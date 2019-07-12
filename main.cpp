bb6299b8841b4b9ab3138530119b6bd5ccad9639
pt207 <47851246+Hay333@users.noreply.github.com>
2019-07-12 09:46:17 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,31 +1,7 @@
-#include <iostream>
-#include <string.h>
-#include <math.h>
-using namespace std;
-int main()
+#include <stdio.h>
+
+int main(int argc, char **argv)
 {
+	printf("hello world\n");
 	return 0;
 }
-
-class String
-{
-	char * value;
-	int len;
-public:
-	String():value(new char[1] {}), len(0) {};//конструктор по умолчанию
-	String(const String& s) {// конструктор копирования
-	delete[] value;
-		len = s.len;
-		value = new char[len + 1];
-		for (int i = 0; i <= len; i++)
-			value[i] = s.value[i];
-	}
-	//перегрузка operator<<
-	friend ostream& operator<<(ostream& outputStream, const String & s) {
-		return outputStream << s.value;
-	}
-	//освобождене памяти
-	~String() {
-		delete[] value;
-	}
-};
